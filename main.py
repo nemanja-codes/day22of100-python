11143119649800ff728062c43bac85e565185ea8
Nemanja Markovic <necamark@gmail.com>
2024-05-09 18:02:37 +0200

    detect collision with paddle

--- main.py
+++ main.py
@@ -28,7 +28,11 @@ while game_is_on:
 
     # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 
 screen.exitonclick()
